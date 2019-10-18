03e5a11dfb63236d7368f4cc4b52d20df13113e1
Shea Newton <shnewto@gmail.com>
2019-10-17 21:44:27 -0700

    use clap to get the filename to run from the command line

--- ashpaper-bin/src/main.rs
+++ ashpaper-bin/src/main.rs
@@ -1,9 +1,20 @@
 extern crate ashpaper;
 
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 use std::fs;
 
 pub fn main() {
-    let fname = "ashpaper-bin/poems/lovely-poem.eso";
+    let matches = App::new("ashpaper")
+        .version("0.1.0")
+        .author("Shea Newton <shnewto@gmail.com>")
+        .about("An AshPaper interpreter that executes poetry!")
+        .args_from_usage("<INPUT>    '.eso file to compile'")
+        .get_matches();
+
+    let fname = matches.value_of("INPUT").unwrap();
     let contents = fs::read_to_string(fname).expect("Something went wrong reading input file!");
     ashpaper::program::execute(&contents);
 }
